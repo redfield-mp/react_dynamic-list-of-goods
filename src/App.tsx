cc14cf6b35d55452bef7192dcc3ed25447334d50
Mykhailo Podaniev <mpodaniev@gmail.com>
2026-02-04 19:56:35 +0200

    Introduce dynamic goods filtering and display logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
 // or
 // import * as goodsAPI from './api/goods';
+import { Good } from './types/Good';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [currentGoods, setCurrentGoods] = useState<Good[]>([]);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  function handleClickAll() {
+    getAll().then(goods => {
+      setCurrentGoods(goods);
+    });
+  }
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  function handleClickFirstFive() {
+    get5First().then(goods => {
+      setCurrentGoods(goods);
+    });
+  }
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  function handleClickRed() {
+    getRedGoods().then(goods => {
+      setCurrentGoods(goods);
+    });
+  }
 
-    <GoodsList goods={[]} />
-  </div>
-);
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button type="button" data-cy="all-button" onClick={handleClickAll}>
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={handleClickFirstFive}
+      >
+        Load 5 first goods
+      </button>
+
+      <button type="button" data-cy="red-button" onClick={handleClickRed}>
+        Load red goods
+      </button>
+
+      <GoodsList goods={currentGoods} />
+    </div>
+  );
+};
